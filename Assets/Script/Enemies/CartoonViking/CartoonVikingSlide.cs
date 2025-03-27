8c6521f251ed8e25ec363384f3107612ba1b564e
EvanWang444 <163755295+EvanWang444@users.noreply.github.com>
2025-03-27 12:09:49 +0800

    2025/03/27 第48次上傳

--- Assets/Script/Enemies/CartoonViking/CartoonVikingSlide.cs
+++ Assets/Script/Enemies/CartoonViking/CartoonVikingSlide.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class CartoonVikingSlide : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.transform.parent.transform.GetComponent<Character>().invulnerableDuration = 1000;
-        animator.transform.parent.transform.GetComponent<Character>().invulnerable = true;
-    }
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -22,8 +21,6 @@ public class CartoonVikingSlide : StateMachineBehaviour
     {
         Rigidbody2D rb = animator.transform.parent.transform.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, rb.velocity.y);
-        animator.transform.parent.transform.GetComponent<Character>().invulnerableDuration = 0;
-        animator.transform.parent.transform.GetComponent<Character>().invulnerable = false;
         animator.transform.parent.transform.GetComponent<CartoonViking>().action = false;
     }
 
